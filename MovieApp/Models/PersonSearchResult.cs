304ab5fd495d4752fd478ff630cb9e43a2817778
Marcell Harmaci <harmaci.marci@gmail.com>
2021-05-10 01:17:04 +0200

    Add Show and Person models

---
+++ MovieApp/Models/PersonSearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+	public class PersonSearchResult
+	{
+		public string Type { get; set; }
+		public float Score { get; set; }
+		public Person Person { get; set; }
+	}
+
+}
---
+++ MovieApp/Models/ShowSearchResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+
+	public class ShowSearchResult
+	{
+		public string Type { get; set; }
+		public float Score { get; set; }
+		public Show Show { get; set; }
+	}
+
+}
