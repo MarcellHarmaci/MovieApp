4a3061410b74edd197f8f6878ede198c25ee1f3a
Marcell Harmaci <harmaci.marci@gmail.com>
2021-05-10 23:16:51 +0200

    Add view displaying show details

---
+++ MovieApp/ViewModels/Details/ShowDetailsPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MovieApp.Models;
+using MovieApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
+
+namespace MovieApp.ViewModels.Details
+{
+	class ShowDetailsPageViewModel : ViewModelBase
+	{
+
+		private ShowDetails showDetails;
+		public ShowDetails ShowDetails
+		{
+			get => showDetails;
+			set
+			{
+				showDetails = value;
+				RaisePropertyChanged(() => ShowDetails);
+			}
+		}
+
+		public override async Task OnNavigatedToAsync(
+			object parameter,
+			NavigationMode mode,
+			IDictionary<string, object> state
+		)
+		{
+			var slug = (string)parameter;
+			var service = new MovieService();
+			ShowDetails = await service.GetShowDetailsAsync(slug);
+			RaisePropertyChanged(() => ShowDetails);
+
+			await base.OnNavigatedToAsync(parameter, mode, state);
+		}
+
+	}
+}
